f44d021e97ffc9500162b04ac801a9e77d1c3783
levihernan <levi.hernan@gmail.com>
2016-11-11 16:41:25 -0300

    yAxis y orden

--- js/highstocks.js
+++ js/highstocks.js
@@ -2,8 +2,8 @@ $(function () {
     $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-ohlcv.json&callback=?', function (data) {
 
         // split the data set into ohlc and volume
-        var ohlc = [],
-            volume = [],
+        var nominal = [],
+            variacion = [],
             dataLength = data.length,
             // set the allowed units for data grouping
             groupingUnits = [[
@@ -17,90 +17,94 @@ $(function () {
             i = 0;
 
         for (i; i < dataLength; i += 1) {
-            ohlc.push([
+            nominal.push([
                 data[i][0], // the date
-                data[i][1], // open
-                data[i][2], // high
-                data[i][3], // low
-                data[i][4] // close
+                data[i][1], //CAMBIAR 1 POR PARAMETRO 2N-1
             ]);
 
-            volume.push([
+            variacion.push([
                 data[i][0], // the date
-                data[i][5] // the volume
+                data[i][2] // CAMBIAR 2 POR PARAMETRO 2N
             ]);
         }
 
 
         // create the chart
         Highcharts.stockChart('chart-container', {
-            rangeSelector: {
-buttons: [ {
-	type: 'month',
-	count: 3,
-	text: '3m'
-}, {
-	type: 'month',
-	count: 6,
-	text: '6m'
-},
-{
-	type: 'year',
-	count: 1,
-	text: '1y'
-},
-{
-	type: 'year',
-	count: 2,
-	text: '2y'
-},
-{
-	type: 'all',
-	text: 'All'
-}],
-                selected: 3,
-                inputEnabled: false,
+              rangeSelector: {
+                buttons: [
+                  {
+                	type: 'month',
+                	count: 3,
+                	text: '3m'
+                }, {
+                	type: 'month',
+                	count: 6,
+                	text: '6m'
+                },
+                {
+                	type: 'year',
+                	count: 1,
+                	text: '1y'
+                },
+                {
+                	type: 'year',
+                	count: 2,
+                	text: '2y'
+                },
+                {
+                	type: 'all',
+                	text: 'All'
+                }],
+                  selected: 3,
+                  inputEnabled: false,
+              },
+						credits: {
+              enabled: true,
+              text: "INDEC",  //PONER FUETE
+              href: '',
             },
-						credits: false,
             title: {
-                text: 'AAPL Historical'
+                text: 'EXPORTACIONES' //PONER TITULO
             },
 
             yAxis: [{
-                labels: {
-                    align: 'right',
-                    x: -3
-                },
-                title: {
-                    text: 'OHLC'
-                },
-                height: '60%',
-                lineWidth: 2
-            }, {
-                labels: {
-                    align: 'right',
-                    x: -3
-                },
-                title: {
-                    text: 'Volume'
-                },
-                top: '65%',
-                height: '35%',
-                offset: 0,
-                lineWidth: 2
-            }],
+               lineWidth: 2,
+               opposite: false,
+               labels: {
+                   align: 'left',
+                   x: 2
+               },
+               title: {
+                   text: 'SERIE' //PONER NOMBRE DE SERIE
+               },
+               height: '60%'
+           },{
+              lineWidth: 2,
+              opposite: false,
+              labels: {
+                  align: 'left',
+                  x: 2
+              },
+              title: {
+                  text: 'VARIACION ANUAL [%]' //CAMBIA?
+              },
+              top: '65%',
+              height: '35%',
+              offset: 0
+          }],
 
             series: [{
                 type: 'line',
                 name: 'AAPL',
-                data: ohlc,
+                data: nominal,
                 dataGrouping: {
                     units: groupingUnits
                 }
             }, {
                 type: 'column',
                 name: 'Volume',
-                data: volume,
+                data: variacion,
                 yAxis: 1,
                 dataGrouping: {
                     units: groupingUnits
