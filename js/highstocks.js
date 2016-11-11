7e48d51dc09127130c4f4863874b8ff43ce64ba3
levihernan <levi.hernan@gmail.com>
2016-11-11 17:54:26 -0300

    add if(no hay variacion) {grafica en 1 solo chart}

--- js/highstocks.js
+++ js/highstocks.js
@@ -4,6 +4,12 @@ $(function () {
     variacion = [0]; //LIMPIA LAS SERIES
 
     $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-ohlcv.json&callback=?', function (data) {
+      Highcharts.setOptions({
+      lang: {
+        months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',  'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        shortMonths: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun',  'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic']
+      }
+    })
 
         // split the data set into ohlc and volume
         var nominal = [],
@@ -34,7 +40,68 @@ $(function () {
 
         //AGREGAR UN IF (NO HAY DATA VAR) {GRAFICAR CON AXIS SIMPLE}
 
+        if( variacion[1][1] = 0 ){
+                Highcharts.stockChart('chart-container', {
+                      rangeSelector: {
+                        buttons: [
+                          {
+                        	type: 'month',
+                        	count: 3,
+                        	text: '3m'
+                        }, {
+                        	type: 'month',
+                        	count: 6,
+                        	text: '6m'
+                        },
+                        {
+                        	type: 'year',
+                        	count: 1,
+                        	text: '1y'
+                        },
+                        {
+                        	type: 'year',
+                        	count: 2,
+                        	text: '2y'
+                        },
+                        {
+                        	type: 'all',
+                        	text: 'All'
+                        }],
+                          selected: 3,
+                          inputEnabled: false,
+                      },
+        						credits: {
+                      enabled: true,
+                      text: "INDEC",  //PONER FUETE
+                      href: '',
+                    },
+                    title: {
+                        text: 'EXPORTACIONES' //PONER TITULO
+                    },
 
+                    yAxis: [{
+                       lineWidth: 2,
+                       opposite: false,
+                       labels: {
+                           align: 'left',
+                           x: 2
+                       },
+                       title: {
+                           text: 'SERIE' //PONER NOMBRE DE SERIE
+                       },
+                       height: '100%'
+                   }],
+
+                    series: [{
+                        type: 'line',
+                        name: 'AAPL',
+                        data: nominal,
+                        dataGrouping: {
+                            units: groupingUnits
+                        }
+                    }]
+                }); //AXIS SIMPLE
+        } else {
         // create the chart
         Highcharts.stockChart('chart-container', {
               rangeSelector: {
@@ -117,5 +184,6 @@ $(function () {
                 }
             }]
         });
+        }
     });
 });
