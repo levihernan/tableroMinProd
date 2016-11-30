96e770c786d7d54815ce2f3c9f53af3b932759a2
Juan Pablo Djeredjian <jpdjeredjian@gmail.com>
2016-11-30 19:12:47 -0300

    Fixed collapsedTablero
    Arreglado el tabler colapasado solo con :before, no es necesario jQuery mobile

--- js/extractTablero.js
+++ js/extractTablero.js
@@ -15,9 +15,22 @@ function draw(dataTablero, tabletop) {
 	console.log("Data Tablero:");
 	console.log(dataTablero);
 
-  function tabulate(data, selection, columns) {
-    //var table = d3.select('#sectorExterno').append('table').attr("style","width:100%")
-    var table = selection.append('table').attr("style","width:100%")
+  function tabulate(data, selection, columns,tipoDeTabla) {
+    /*Argumentos: data: data a incluir. Cortar la data necesaria, como se ve en los llamados abajo.
+                  selection: elemento adonde meter la tabla
+                  columnas: elegir columnas a graficar
+                  tipoDeTabla: true para tabla grande, false para tabla comprimida
+    */
+
+    if(tipoDeTabla){
+      var table = selection.append('table').attr("style","width:100%");
+    }else{
+      var table = selection.append('table')
+                      .attr("id","collapsed-table")
+                      .attr("data-mode","reflow")
+                      .attr("class","ui-responsive ui-table-reflow table-stroke")
+    }
+ 
     var thead = table.append('thead')
     var tbody = table.append('tbody');
 
@@ -39,20 +52,13 @@ function draw(dataTablero, tabletop) {
       .data(function (row) {
 
         return columns.map(function (column) {
-          // console.log("-----------------");
-          // console.log("row");
-          // console.log(row);
-          // console.log("column");
-          // console.log(column);
-          // console.log("row[column]");
-          // console.log(row[column]);
 
           //Las columnas que estn en filledColumns
           if(jQuery.inArray(column,filledColumns) !== -1){
             return {column: column, value: row[column]};
           }//Las columnas first and last
           else if(jQuery.inArray(column,firstAndLastColumns) !== -1){
-            return {column: column, value: row[column], class:"firstAndLast"};
+            return {column: column, value: row[column]};
           }else //Las demas (son las que tiene valores fijos)
           {
             return {column: column, value: "free space",id:"unidad"};
@@ -62,13 +68,33 @@ function draw(dataTablero, tabletop) {
       .enter()
       .append('td')
         .text(function (d) { return d.value; })
-        .attr("class",function (d) { return d.class})
+        .attr("class",function (d,i) { 
+  
+            return "number"+i;
+
+        })
         .attr("id",function (d,i) {
           if(d.id !== undefined){
             return d.id+""+i;
           } 
-          
         });
+
+
+
+        if(tipoDeTabla === false){// en caso de que corra la funcion para collapsed, creo headers
+
+          headRows = thead.selectAll('tr')
+          .data([0])
+          .enter()
+          .append('tr');
+
+          headCells = headRows.selectAll('th')
+          .data([1,2,3,4,5,6,7,8])
+          .enter()
+          .append('th')
+          .text(function (d) {return d;});
+
+        }//cierro if
 
     return table;
   }	
@@ -77,31 +103,36 @@ function draw(dataTablero, tabletop) {
   //las columnas las tengo que incluir para guardar el espacio para ellas, incluso a las que no les paso data
   dataSectorExt = dataTablero.slice(0,6);
   selection = d3.select('#sectorExterno');
-  tabulate(dataSectorExt, selection, ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15']); // 2 column table
+  tabulate(dataSectorExt, selection, ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15'],true); // 2 column table
 
   dataInversion = dataTablero.slice(6,12);
   selection = d3.select('#inversion');
-  tabulate(dataInversion, selection, ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15']); // 2 column table
+  tabulate(dataInversion, selection, ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15'],true); // 2 column table
 
   dataConsumo = dataTablero.slice(12,17);
   selection = d3.select('#consumo');
-  tabulate(dataConsumo, selection,  ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15']); // 2 column table
+  tabulate(dataConsumo, selection,  ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15'],true); // 2 column table
 
   dataPrecios = dataTablero.slice(17,23);
   selection = d3.select('#precios');
-  tabulate(dataPrecios, selection,  ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15']); // 2 column table
+  tabulate(dataPrecios, selection,  ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15'],true); // 2 column table
 
   dataSectorReal = dataTablero.slice(23,33);
   selection = d3.select('#sectorReal');
-  tabulate(dataSectorReal,  selection, ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15']); // 2 column table
+  tabulate(dataSectorReal,  selection, ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15'],true); // 2 column table
 
   dataMonetario = dataTablero.slice(33,36);
   selection = d3.select('#monetario');
-  tabulate(dataMonetario,  selection, ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15']); // 2 column table
+  tabulate(dataMonetario,  selection, ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15'],true); // 2 column table
 
   dataFiscal = dataTablero.slice(36,40);
   selection = d3.select('#fiscal');
-  tabulate(dataFiscal, selection,  ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15']); // 2 column table
+  tabulate(dataFiscal, selection,  ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15'],true); // 2 column table
+  
+  /*-----Data para tablero comprimido ----------------*/
+  dataTotal = dataTablero.slice(0,40);
+  selection = d3.select('#collapsedTableroContainer');
+  tabulate(dataTotal, selection,  ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15'],false); // 2 column table
 
   $('#sectorExterno #unidad1').html('Millones de U$S');
   $('#sectorExterno #unidad5').html('Anual');
@@ -129,6 +160,11 @@ function draw(dataTablero, tabletop) {
   $('#monetario #unidad5').html('Acum.');
   $('#fiscal #unidad1').html('Millones de U$S');
   $('#fiscal #unidad5').html('Anual en %');
+
+  // Agrego separadores de "Ultimo dato disponible" y "Acumulado" en CollapsedTable
+  $('#collapsedTableroContainer #collapsed-table td.number2').before('<td class="udd">Último dato disponible</td>')
+  $('#collapsedTableroContainer #collapsed-table td.number5').before('<td class="acumulado">Acumulado</td>')
+
 }
 
 renderSpreadsheetData();
